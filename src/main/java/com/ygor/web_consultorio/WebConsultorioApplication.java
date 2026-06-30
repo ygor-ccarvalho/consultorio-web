71bf45d00e61e679abf38f3d5dbfecc343aadeaf
Ygor César de Carvalho <109998784+ygor-ccarvalho@users.noreply.github.com>
2026-06-30 17:02:51 -0300

    Chore: setup inicial do projeto Spring Boot

---
+++ src/main/java/com/ygor/web_consultorio/WebConsultorioApplication.java
@@ -0,0 +1,13 @@
+package com.ygor.web_consultorio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebConsultorioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebConsultorioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ygor/web_consultorio/WebConsultorioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ygor.web_consultorio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebConsultorioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
