71168de1c70063c1b3517253ac7d616e53f1b091
Ygor César de Carvalho <109998784+ygor-ccarvalho@users.noreply.github.com>
2026-07-08 15:57:14 -0300

    feat: adiciona busca por CPF no PacienteRepository

--- src/main/java/com/ygor/web_consultorio/repository/PacienteRepository.java
+++ src/main/java/com/ygor/web_consultorio/repository/PacienteRepository.java
@@ -1,9 +1,15 @@
 package com.ygor.web_consultorio.repository;
 
+
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ygor.web_consultorio.model.Paciente;
 
 public interface PacienteRepository extends JpaRepository<Paciente, Long> {
+
+	Optional<Paciente> findByCpf(String cpf);
+	
 
 }
