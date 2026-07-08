066443579430cb85a93ab9fa694cdf6136ab8a29
Ygor César de Carvalho <109998784+ygor-ccarvalho@users.noreply.github.com>
2026-07-08 15:57:16 -0300

    feat: implementa create com validacao de CPF no PacienteService

--- src/main/java/com/ygor/web_consultorio/service/PacienteService.java
+++ src/main/java/com/ygor/web_consultorio/service/PacienteService.java
@@ -1,12 +1,14 @@
 package com.ygor.web_consultorio.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
 import com.ygor.web_consultorio.dto.PacienteDTO;
 import com.ygor.web_consultorio.model.Paciente;
 import com.ygor.web_consultorio.repository.PacienteRepository;
+import com.ygor.web_consultorio.service.exception.DataBindingViolationException;
 import com.ygor.web_consultorio.service.exception.ObjectNotFoundException;
 
 import lombok.RequiredArgsConstructor;
@@ -27,9 +29,22 @@ public class PacienteService {
 		return new PacienteDTO(buscarEntidade(id));
 		}
 	
+	public PacienteDTO create(PacienteDTO objDTO) {
+		objDTO.setId(null);
+		validaPorCpf(objDTO);
+		Paciente newObj = repository.save(new Paciente(objDTO));
+		return new PacienteDTO(newObj);
+	}
+	
 	public Paciente buscarEntidade(Long id) {
 		return repository.findById(id)
 				.orElseThrow(() -> new ObjectNotFoundException("Paciente não encontrado: " + id));
 	}
 	
+	private void validaPorCpf(PacienteDTO objDTO) {
+	Optional<Paciente> obj = repository.findByCpf(objDTO.getCpf()); 
+	if (obj.isPresent() && !obj.get().getId().equals(objDTO.getId())) {
+		throw new DataBindingViolationException("CPF já cadastrado no sistema!");
+	}
+	}
 }
